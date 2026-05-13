8fed83d65c7365406ccc8b73e13aedf26c370f7f
evodi-21 <ephinince@gmail.com>
2026-05-13 11:02:56 +0300

    Fix: Explicitly disabled SecurityAutoConfiguration to stop redirect loop

--- demo/src/main/java/com/amatuer3/demo/DemoApplication.java
+++ demo/src/main/java/com/amatuer3/demo/DemoApplication.java
@@ -2,13 +2,11 @@ package com.amatuer3.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
